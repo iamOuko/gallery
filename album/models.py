49030920a666ba4579d4f6c977f99127cf89644e
iamOuko <oukoezzy@gmail.com>
2020-10-12 11:13:47 +0300

    first commit

---
+++ album/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = '')
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update()
+
+
